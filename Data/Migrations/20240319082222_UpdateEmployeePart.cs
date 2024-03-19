66b8091fc2408f7de4fd963d13bbada1654e2ec1
楊鋼 <sobeautiful0909@gmail.com>
2024-03-19 16:27:36 +0800

    Update Employee Entity Fields

---
+++ Data/Migrations/20240319082222_UpdateEmployeePart.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NCG.HR.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEmployeePart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityId",
+                table: "Employees",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_CityId",
+                table: "Employees",
+                column: "CityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Cities_CityId",
+                table: "Employees",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Cities_CityId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_CityId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
